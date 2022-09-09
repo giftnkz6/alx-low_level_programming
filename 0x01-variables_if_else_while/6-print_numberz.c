81d060edffbf24e5de8531510e5a82084ac9c1fc
Gift Mahlangu <giftnkz6@gmail.com>
2022-09-09 14:00:20 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int t;
+
+	for (t =0; t < 10; t++)
+	{
+		putchar(t);
+	}
+	putchar('\n');
+	return (0);
+}
