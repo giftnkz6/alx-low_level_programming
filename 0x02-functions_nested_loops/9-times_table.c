851c9788f673df335024a69306dac439e607f7cb
Gift Mahlangu <giftnkz6@gmail.com>
2022-09-14 08:43:20 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,6 +7,7 @@
 void times_table(void)
 {
 	int a, b, c;
+
 	for (a = 0; a <= 9; a++)
 	{
 		_putchar(48);
@@ -26,7 +27,7 @@ void times_table(void)
 				_putchar((c % 10) + 48);
 			}
 		}
-		_putchar('\n');
+	_putchar('\n');
 	
 	}
 }
