bfd400fde01f31a863dacafe453f9ffbc4be6874
Gift Mahlangu <giftnkz6@gmail.com>
2022-09-15 06:50:28 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,7 +5,6 @@
  * Return: 0
  */
 
-
 int main(void)
 {
 	int i;
@@ -14,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	/*while (i < 10)
+	while (i < 10)
 	{
 		putchar(i);
-	}*/
+	}
 
 	printf("Infinite loop avoided! \\o/\n");
 
